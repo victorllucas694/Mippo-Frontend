c8c912919d0f0f48868dd78359272f43eedaf212
victorllucas694 <victorllucas.silva01@gmail.com>
2024-01-23 15:59:52 -0300

    PaymentPage

--- src/pages/Payment/Checkout/index.tsx
+++ src/pages/Payment/Checkout/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
-import { ModalBox, PaperBox, PaymentAllSteps } from "./styles";
+import { PaymentAllSteps } from "./styles";
 import { useAxios } from "../../../providers/AxiosProvider";
 import { useAuth } from "../../../contexts/AuthenticateContext";
 import AddIcon from "@mui/icons-material/Add";
@@ -19,129 +19,79 @@ import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import AddressForm from "../AddressForm";
 import { useState } from "react";
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
-import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
-
-interface CartProduct {
-  getProductsByOrderId: Record<string, unknown>;
-}
+import ProductDetails from "../../../components/Global/ProductDetails";
+import Textarea from "@mui/joy/Textarea";
+import Stack from "@mui/joy/Stack";
 
 export default function Checkout() {
-  const { axiosInstance } = useAxios();
   const { id } = useAuth();
-  const [cartProducts, setCartProducts] = React.useState<CartProduct[]>([]);
-  const [totalPrice, setTotalPrice] = React.useState<number>(0);
-  const [totalOfferPrice, setTotalOfferPrice] = React.useState<number>(0);
+  const { axiosInstance } = useAxios();
+  const token = localStorage.getItem("c__token");
 
   React.useEffect(() => {
-    getCartProducts();
-    setterTotalPrice();
-    setterTotalPriceWithOffer();
-    getPaperList();
+    getInventaryUserProducts(id);
   }, []);
 
-  const setterTotalPrice = async () => {
-    const products = await axiosInstance.get(
-      `/payment-shipping-cart/cart/${id}`
+  const [getOrderProducts, setGetOrderProducts] = useState([]);
+
+  const getInventaryUserProducts = async (id: number) => {
+    const getOrderProducts = await axiosInstance.get(
+      `/payment-shipping-cart/cart/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
     );
-
-    let arrToPrices: number = 0;
-
-    products.data.map((products: { getProductsByOrderId: any }) => {
-      const { getProductsByOrderId } = products;
-      arrToPrices += parseFloat(getProductsByOrderId.Valor_a_prazo);
-    });
-    setTotalPrice(arrToPrices);
+    setGetOrderProducts(getOrderProducts.data);
+    console.log("getOrderProducts", getOrderProducts.data);
   };
+  const textareaRef = React.useRef<HTMLTextAreaElement | null>(null);
 
-  const [totalPriceFiltredData, setTotalPriceFiltredData] = useState<number>(0);
-
-  const setterTotalPriceWithOffer = async () => {
-    const products = await axiosInstance.get(
-      `/payment-shipping-cart/cart/${id}`
-    );
-
-    let arrToOfferPrices: number = 0;
-    let totalAmount: number = 0;
-    console.log(products);
-    products.data.map((products: { getProductsByOrderId: any }) => {
-      const { getProductsByOrderId } = products;
-
-      const cleanNumber = getProductsByOrderId.Valor_a_vista.replace(
-        /[^\d.]/g,
-        ""
-      ).replace(",", ".");
-      const number = parseFloat(cleanNumber);
-
-      totalAmount += number;
-
-      setTotalPriceFiltredData(totalAmount);
-    });
-    setTotalOfferPrice(arrToOfferPrices);
+  const handleTextareaFocus = () => {
+    textareaRef.current?.focus();
   };
-
-  const getCartProducts = async () => {
-    const products = await axiosInstance.get(
-      `/payment-shipping-cart/cart/${id}`
-    );
-
-    console.log("products", products);
-
-    console.log(products)
-
-    setCartProducts(products.data);
-  };
-
-  const { paymentData } = useMyContext();
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  // const style = {
-  //   position: "absolute" as "absolute",
-  //   top: "50%",
-  //   left: "50%",
-  //   transform: "translate(-50%, -50%)",
-  //   width: 500,
-  //   bgcolor: "background.paper",
-  //   border: "2px solid #000",
-  //   boxShadow: 24,
-  //   p: 4,
-  // };
-
-  interface IPaperProduct {
-    CEP: string;
-    User_Id: string;
-    address: string;
-    address_other: string;
-    city: string;
-    country: string;
-    id: string;
-    last_name: string;
-    name: string;
-    state: string;
-  }
-
-  const [paperProducts, setPaperProducts] = useState<IPaperProduct[] | null>(
-    null
-  );
-
-  const getPaperList = async () => {
-    const token = localStorage.getItem("c__token");
-    const papers = await axiosInstance.get(`/user-address/get-all/${id}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    setPaperProducts(papers.data);
-  };
-
   return (
     <React.Fragment>
-      
+      <PaymentAllSteps>
+        <div className="body-cards">
+          {getOrderProducts.map((cards) => {
+            return <ProductDetails getOrderProducts={cards} />;
+          })}
+        </div>
+        <div className="body-cards">
+          <div className="cart-values">
+            <div className="header-cart">
+              <h1>Resgate seu cupom</h1>
+            </div>
+            <div className="shipping-cart-code">
+              <TextField
+                required
+                id="name"
+                name="name"
+                label="Código de desconto"
+                fullWidth
+                type="text"
+                autoComplete="shipping address-level2"
+                variant="outlined"
+                sx={{
+                  width: "70%",
+                  margin: "auto",
+                }}
+              />
+              <Button
+                sx={{ width: "25%", height: "3rem", margin: "auto" }}
+                variant="outlined"
+              >
+                aplicar
+              </Button>
+            </div>
+            <div className="prices-values"> 
+                
+            </div>
+          </div>
+        </div>
+      </PaymentAllSteps>
     </React.Fragment>
   );
-
 }
--- src/pages/Payment/Checkout/styles.ts
+++ src/pages/Payment/Checkout/styles.ts
@@ -2,61 +2,59 @@ import styled from "styled-components";
 
 export const PaymentAllSteps = styled.div`
   width: 100%;
-  min-height: 80vh;
+  height: auto;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
+  gap: 1rem;
+  margin: 4rem 0;
 
-  .products-data-payment {
-    width: 50%;
-    border: 1px solid;
-    height: 80vh;
-    min-width: 45rem;
-    margin: auto;
+  .body-cards {
+    width: 48%;
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    gap: 2rem;
+    min-height: 40rem;
+    height: auto;
+    padding: 1rem;
 
-    .products-by-express {
-      width: 100%;
+    .prices-values {
+      width: 90%;
+      margin: 1rem auto;
+      height: auto;
+      min-height: 16rem;
       border: 1px solid;
-      height: 80vh;
-      display: flex;
-      justify-content: center;
-      align-items: center;
     }
 
-    .header-products {
-      width: 100%;
-      height: 5rem;
-      border: 1px solid;
-      display: flex;
-      align-items: center;
+    .cart-values {
+      width: 80%;
+      height: auto;
+      margin: 0 auto;
+      min-height: 30rem;
+      box-shadow: 3px 3px 16px -8px rgb(220, 220, 220);
+      border: 1px solid rgb(230, 230, 230);
 
-      .circle-data {
-        box-shadow: 2px 2px 10px rgb(240, 240, 240);
-        border: 1px solid rgb(230, 230, 230);
-        margin: 0.4rem 5rem;
-        width: 4rem;
-        border-radius: 4rem;
-        height: 4rem;
+      .header-cart {
+        min-height: 4rem;
+        width: 90%;
+        margin: 1rem auto;
+        border-bottom: 1px solid rgb(230, 230, 230);
+
+        h1 {
+          font-size: 1.6rem;
+          font-family: 'Roboto';
+          margin: 2rem;
+        }
+      }
+
+      .shipping-cart-code {
+        width: 90%;
+        margin: auto;
         display: flex;
-        justify-content: center;
-        align-items: center;
+        justify-content: space-between;
+        height: 4rem;
       }
 
-      h1 {
-        font-family: "Open Sans";
-        font-size: 1.2rem;
-        font-weight: 300;
-      }
     }
-  }
-
-  .products-data {
-    border: 1px solid;
-    width: 50%;
-    height: 80vh;
   }
 `;
